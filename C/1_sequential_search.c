7cd887c0bd60582e850c219033561eaff201b920
code1023 <75021283+code1023@users.noreply.github.com>
2021-04-18 01:16:25 +0900

    Create 1_sequential_search.c

---
+++ C/1_sequential_search.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int LinearSearch(int arr[], int len, int target)
+{
+    int i;
+    for (i = 0; i < len; i++) 
+    {
+        if (arr[i] == target)
+            return i;       // 찾은 대상의 인덱스 값 반환
+    }
+    return -1;      // 찾지 못했음을 의미하는 값 반환
+}
+
+int main(void)
+{
+    int arr[] = {3, 5, 2, 4, 9};
+    int idx;
+
+    idx = LinearSearch(arr, sizeof(arr)/sizeof(int), 4);
+
+    if (idx == -1)
+        printf("탐색 실패!\n");
+    else
+        printf("타겟 저장 인덱스: %d \n", idx);
+    
+    return 0;
+}
