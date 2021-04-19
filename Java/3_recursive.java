8ad20e4483d2676f2ceb75d5329f53949de4fab6
code1023 <75021283+code1023@users.noreply.github.com>
2021-04-20 00:59:06 +0900

    Create 3_recursive.java

---
+++ Java/3_recursive.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    // 반복적으로 구현
+    public static int factorialIterative(int n) {
+        int result = 1;
+
+        for (int i = 1; i < n + 1; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    // 재귀적으로 구현
+    public static int factorialRecursive(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        return n * factorialRecursive(n - 1);
+    }
+  
+    // main
+    public static void main(String[] args) {
+        System.out.println("반복적으로 구현: " + factorialIterative(5));
+        System.out.println("재귀적으로 구현: " + factorialRecursive(5));
+    }
+
+}
