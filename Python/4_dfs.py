506c261fe42ab40fd2cbd7d8fee1c1f0fd1a8554
code1023 <75021283+code1023@users.noreply.github.com>
2021-04-20 18:39:37 +0900

    Create 4_dfs.py

---
+++ Python/4_dfs.py
@@ -0,0 +1,28 @@
+# DFS 메서드 정의
+def dfs(graph, v, visited):
+    # 현재 노드를 방문 처리
+    visited[v] = True
+    result.append(v)
+
+    # 현재 노드와 연관된 다른 노드를 재귀적으로 방문 
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+result = []
+visited = [False] * 9
+
+dfs(graph, 1, visited)
+print(result)
