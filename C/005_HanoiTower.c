5f199b8eefd3d70b868759b4739765b6a238ceea
code1023 <75021283+code1023@users.noreply.github.com>
2021-04-21 20:40:12 +0900

    Create 005_HanoiTower.c

---
+++ C/005_HanoiTower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int HanoiTowerMove(int num, char from, char by, char to) {
+    if (num == 1)
+        printf("원반1를 %c에서 %c로 이동 \n", from, to);
+    else
+    {
+        HanoiTowerMove(num - 1, from, to, by);
+        printf("원반%d을(를) %c에서 %c로 이동 \n", num, from, to);
+        HanoiTowerMove(num - 1, by, from, to);
+    }
+}
+
+int main() {
+    HanoiTowerMove(4, 'A', 'B', 'C');
+    
+    return 0;
+}
