bd20b4999c90362e02d91a186f8713cc8284d616
code1023 <75021283+code1023@users.noreply.github.com>
2021-05-23 12:05:27 +0900

    Update insertion_sort.py

--- Python/sort/insertion_sort.py
+++ Python/sort/insertion_sort.py
@@ -1,10 +1,11 @@
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-for i in range(1, len(array)):
-    for j in range(i, 0, -1):
-        if array[j - 1] > array[j]:
-            array[j - 1], array[j] = array[j], array[j - 1]
-        else:
-            break
+def ins_sort(array):
+	for i in range(1, len(array)):
+		for j in range(i, 0, -1):
+			if array[j - 1] > array[j]:
+				array[j - 1], array[j] = array[j], array[j - 1]
+			else:
+				break
 
-print(array)
+	return array
